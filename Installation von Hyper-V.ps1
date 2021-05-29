00770e6f7bc999413f3591332aa640dcbf2d7e6e
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-05-29 07:24:35 +0200

    Add files via upload

---
+++ Installation von Hyper-V.ps1
@@ -0,0 +1,7 @@
+﻿#Installation von Hyper-V
+
+#To install Hyper-V on a server you're connected to remotely
+Install-WindowsFeature -Name Hyper-V -ComputerName m4700 -IncludeManagementTools -Restart
+
+#After the server restarts, you can see that the Hyper-V role is installed
+Get-WindowsFeature -ComputerName m4700
