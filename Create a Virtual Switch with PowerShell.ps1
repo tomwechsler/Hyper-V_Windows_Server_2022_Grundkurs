d8f133ad3289816f56b682a85817539edab74839
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-06-05 06:40:52 +0200

    Add files via upload

---
+++ Create a Virtual Switch with PowerShell.ps1
@@ -0,0 +1,13 @@
+﻿Set-location c:\
+Clear-Host
+
+#Create a Virtual Switch with PowerShell
+
+#Find the interface name
+Get-NetAdapter
+
+#Create a variable
+$net = Get-NetAdapter -Name 'Management'
+
+#Create the switch
+New-VMSwitch -Name "External VM Switch" -AllowManagementOS $True -NetAdapterName $net.Name
