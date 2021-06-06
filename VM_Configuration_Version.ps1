baa58f36ff8396d975bb95f604baad76271e0be4
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-06-06 07:15:22 +0200

    Add files via upload

---
+++ VM_Configuration_Version.ps1
@@ -0,0 +1,14 @@
+﻿#Check the virtual machine configuration versions
+Get-VM * | Format-Table Name, Version
+
+#Upgrade the virtual machine configuration version
+Update-VMVersion "WindowsCV8"
+
+#Supported virtual machine configuration versions
+Get-VMHostSupportedVersion -Default
+
+#create a virtual machine named "WindowsCV8" with a configuration version 8.0
+New-VM -Name "WindowsCV8" -Version 8.0
+
+#Check the virtual machine configuration versions
+Get-VM * | Format-Table Name, Version
