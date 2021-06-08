128c58eaa1accda239da465e3d5ebcf6cc093356
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-06-08 06:32:12 +0200

    Add files via upload

---
+++ Nested_VM.ps1
@@ -0,0 +1,13 @@
+﻿#Configure Nested Virtualization
+Set-VMProcessor -VMName <VMName> -ExposeVirtualizationExtensions $true
+
+#Disable Nested Virtualization
+Set-VMProcessor -VMName <VMName> -ExposeVirtualizationExtensions $false
+
+#MAC Address Spoofing
+Get-VMNetworkAdapter -VMName <VMName> | Set-VMNetworkAdapter -MacAddressSpoofing On
+
+#Network Address Translation (NAT)
+New-VMSwitch -Name VmNAT -SwitchType Internal
+New-NetNat –Name LocalNAT –InternalIPInterfaceAddressPrefix “192.168.100.0/24”
+Get-NetAdapter "vEthernet (VmNat)" | New-NetIPAddress -IPAddress 192.168.100.1 -AddressFamily IPv4 -PrefixLength 24
